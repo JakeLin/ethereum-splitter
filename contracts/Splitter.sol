67419fa72a09f24ffc4f385918cd499d905aba0a
Jake Lin <jakelinau@gmail.com>
2019-07-20 11:01:03 +1000

    Add `Splitter` contract

---
+++ contracts/Splitter.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract Splitter {
+  address public alice;
+  address payable public bob;
+  address payable public carol;
+
+  constructor(address _alice, address payable _bob, address payable _carol) public {
+    alice = _alice;
+    bob = _bob;
+    carol = _carol;
+  }
+
+  modifier onlyAlice() {
+    require (msg.sender == alice, "Only alice can split!");
+    _;
+  }
+
+  function split() external payable onlyAlice() {
+    require(msg.value > 0, "Must split more than zero ether!");
+    require(msg.value % 2 == 0, "The ether to be splitted must be even!");
+    uint256 half = msg.value / 2;
+    bob.transfer(half);
+    carol.transfer(half);
+  }
+}
